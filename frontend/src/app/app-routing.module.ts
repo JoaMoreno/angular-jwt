fca37969d5edcf02cd1c74147873c6e93ea54262
Joaquin Moreno <tec.ignaciomoreno@gmail.com>
2020-02-25 17:05:54 -0300

    First Commit Frontend

---
+++ frontend/src/app/app-routing.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+// Components
+import { PublicTaskComponent } from './auth/public-task/public-task.component';
+import { PrivateTaskComponent } from './auth/private-task/private-task.component';
+import { SinginComponent } from './auth/singin/singin.component';
+import { SingupComponent } from './auth/singup/singup.component';
+
+const routes: Routes = [
+  {path: '', redirectTo: '/tasks', pathMatch: 'full'},
+  {path: 'tasks', component: PublicTaskComponent},
+  {path: 'private', component: PrivateTaskComponent},
+  {path: 'singin', component: SinginComponent},
+  {path: 'singup', component: SingupComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
